d55c5f783a55ef553a43d136ebc2461686bb4278
liujiaqi7998 <1792498113@qq.com>
2022-08-30 11:33:07 +0800

    实体添加与删除监测

---
+++ CreateEntity.js
@@ -0,0 +1,61 @@
+module.exports = function(RED) {
+
+    
+
+    function CreateEntity(config) {
+
+        RED.nodes.createNode(this, config);
+        this.server = RED.nodes.getNode(config.server);
+
+        this.on('input', function (msg) {
+            if (this.server) {
+                //在服务器中注册该节点，以便于回调
+                this.server.rec_add(this.id, this);
+
+                //建立发送信息的JSON模板
+                var temp_msg = {};
+                temp_msg['type'] = "CreateEntity";
+                temp_msg['id'] = msg.payload['id'];
+                temp_msg['amount'] = msg.payload['amount'];
+                temp_msg['level'] = msg.payload['level'];
+                temp_msg['player_uid'] = msg.payload['player'];
+                
+
+                temp_msg['msg_id'] = this.id;
+
+                //发送信息
+                this.server.send(JSON.stringify(temp_msg).toString());
+            } else {
+                this.warn("未配置服务器");
+            }
+        });
+
+        this.on('close', function () {
+            // 从接收监听器中注销
+            this.server.rec_del(this.id);
+        });
+
+        // 处理回调函数
+        this.deal = function (temp) {
+
+            // 删除该节点，防止重复响应问题
+            this.server.rec_del(this.id);
+
+            // 判断返回数据是否正常
+            if (temp['type'] === "error") {
+                this.error(temp['data']);
+                return;
+            } 
+
+            //新建标准返回格式
+            var msg = {};
+            msg['payload'] = temp['data'];
+
+            //调用节点输出
+            this.send(msg);
+            
+        }
+    }
+
+    RED.nodes.registerType("CreateEntity",CreateEntity);
+}
---
+++ OnPosition.js
@@ -0,0 +1,67 @@
+module.exports = function(RED) {
+
+    
+
+    function OnPosition(config) {
+
+        RED.nodes.createNode(this, config);
+        this.server = RED.nodes.getNode(config.server);
+
+        this.on('input', function (msg) {
+            if (this.server) {
+                //在服务器中注册该节点，以便于回调
+                this.server.rec_add(this.id, this);
+
+                //建立发送信息的JSON模板
+                var temp_msg = {};
+                temp_msg['type'] = "OnPosition";
+                temp_msg['X'] = parseFloat(msg.payload['X']);
+                temp_msg['Y'] = parseFloat(msg.payload['Y']);
+                temp_msg['Z'] = parseFloat(msg.payload['Z']);
+                temp_msg['scene'] = parseInt(msg.payload['scene']);
+                temp_msg['R'] = parseFloat(msg.payload['R']);
+                temp_msg['msg_id'] = this.id;
+
+                //发送信息
+                this.server.send(JSON.stringify(temp_msg).toString());
+            } else {
+                this.warn("未配置服务器");
+            }
+        });
+
+        this.on('close', function () {
+            // 发送注销监听事件
+            var temp_msg = {};
+            temp_msg['type'] = "OnPosition";
+            temp_msg['msg_id'] = this.id;
+            temp_msg['del'] = "1";
+            this.server.send(JSON.stringify(temp_msg).toString());
+
+            // 从接收监听器中注销
+            this.server.rec_del(this.id);
+        });
+
+        // 处理回调函数
+        this.deal = function (temp) {
+
+            // 删除该节点，防止重复响应问题
+            this.server.rec_del(this.id);
+
+            // 判断返回数据是否正常
+            if (temp['type'] === "error") {
+                this.error(temp['data']);
+                return;
+            } 
+
+            //新建标准返回格式
+            var msg = {};
+            msg['payload'] = temp['data'];
+
+            //调用节点输出
+            this.send(msg);
+            
+        }
+    }
+
+    RED.nodes.registerType("OnPosition",OnPosition);
+}
